d20e98d82f89e5bf22ddfff0cd8ff41273a492ed
MohamedMostafa11 <mohamedmostafa11112000@gmail.com>
2024-11-15 15:06:53 +0200

    Add_Employer_model

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employer;
+use App\Models\Job;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class JobFactory extends Factory
+{
+
+    public function definition()
+    {
+        //
+    }
+}
