5e4d26bf67c1ea61c33dbb3eaf7722bd996f931e
MohamedMostafa11 <mohamedmostafa11112000@gmail.com>
2024-12-01 13:36:06 +0200

    submit form and CSRF

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -11,7 +11,8 @@ class Job extends Model
     /** @use HasFactory<\Database\Factories\JobFactory> */
     use HasFactory;
     protected $table = 'job_listings';
-    protected $fillable = ['title', 'salary', 'employer_id'];
+    //protected $fillable = ['title', 'salary', 'employer_id'];
+    protected $guarded = [];
 
     function employer(): BelongsTo
     {
--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,42 @@ Route::get('/', function () {
     return view('home');
 });
 
+/************** Job ******************/
+
 Route::get('/jobs', function () {
     //$jobs = Job::all();
     //$jobs = Job::with('employer')->paginate(3); //to minimize the number of query
     //$jobs = Job::with('employer')->cursorPaginate(3);
-    $jobs = Job::with('employer')->simplePaginate(3);
-    return view('jobs',[
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+    return view('jobs.index',[
         'jobs'=> $jobs
     ]);
 });
 
+Route::get('/jobs/create', function (){
+  return view('jobs.create');
+});
+
 Route::get('/job/{id}', function ($id) {
     $job=Job::find($id);
-    return view('job',['job'=> $job]);
+    return view('jobs.show',['job'=> $job]);
 });
+
+Route::post('/jobs',function (){
+    //dd(request()->all());
+    //validation...
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+    return redirect('/jobs');
+
+});
+
+/************** contact ******************/
+
 
 Route::get('/contact', function () {
     return view('contact');
